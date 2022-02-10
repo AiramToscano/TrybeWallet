90407c9edbc638b76d33b76d49e64b59aef65ecc
Airam Toscano <airamlobato@gmail.com>
2022-02-10 16:31:01 -0400

    exercicio 6 finalizado

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -151,18 +151,22 @@ class Wallet extends React.Component {
           <button type="button" onClick={ this.onSubmitDespesas }>
             Adicionar Despesas
           </button>
-          {/* <div>
-          <span>Descrição</span>
-          <span>Tag</span>
-          <span>Método de pagamento</span>
-          <span>Valor</span>
-          <span>Moeda</span>
-          <span>Câmbio utilizado</span>
-          <span>Valor convertido</span>
-          <span>Moeda de conversão</span>
-          <span>Editar/Excluir</span>
-        </div> */}
         </header>
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+        </table>
       </div>
     );
   }
